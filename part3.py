da4602ea29ccb4f427e6cf5101766d3b27b3aad6
Herbert Wang <rwang67@siebl-0222-27.ews.illinois.edu>
2016-11-11 19:01:18 -0600

    Part3.py

--- part3.py
+++ part3.py
@@ -3,7 +3,7 @@ from copy import copy, deepcopy
 import time, sys
 from collections import defaultdict as setdefault
 import itertools
-import ipdb
+# import ipdb
 def main():
 	with open('rt-train.txt') as f:
 			content_movie = f.readlines()
@@ -29,41 +29,41 @@ def main():
 	if arg == "1":
 		print("Multinomial Navie Bayes")
 		
+		# best_accuracy = 0
+		# best_laplace = 0
+		# c = [0.5, 1, 2, 5, 10, 100, 1000, 5000]
+		# for i in c:
+		# 	pclass = {}
+		# 	nclass = {}
+		# 	accuracy = 0
+		# 	Build_Mult(content_movie, pclass, nclass, i)
+		# 	accuracy = Classify_Mult(content_movie_testing, pclass, nclass, movie_p_prior, movie_n_prior)
+		# 	# import IPython
+		# 	# IPython.embed()
+		# 	# exit()
+		# 	print("Accuracy ", i, ": ", accuracy, "%")
+		# 	if best_accuracy < accuracy:
+		# 		best_accuracy = accuracy
+		# 		best_laplace = i
+		# print("Best Laplace: ", best_laplace)
+		# print("Movie Reviews Accuracy: ", best_accuracy, "%")
+
+
 		best_accuracy = 0
 		best_laplace = 0
-		c = [1, 2, 5, 10, 100, 1000, 9075]
+		c = [0.5, 1, 2, 5, 10, 100, 1000, 5000]
 		for i in c:
 			pclass = {}
 			nclass = {}
 			accuracy = 0
-			Build_Mult(content_movie, pclass, nclass, i)
-			accuracy = Classify_Mult(content_movie_testing, pclass, nclass, movie_p_prior, movie_n_prior)
-			# import IPython
-			# IPython.embed()
-			# exit()
+			Build_Mult(content_topic, pclass, nclass, i)
+			accuracy = Classify_Mult(content_topic_testing, pclass, nclass, topic_p_prior, topic_n_prior)
 			print("Accuracy ", i, ": ", accuracy, "%")
 			if best_accuracy < accuracy:
 				best_accuracy = accuracy
 				best_laplace = i
 		print("Best Laplace: ", best_laplace)
-		print("Movie Reviews Accuracy: ", best_accuracy, "%")
-
-
-		# best_accuracy = 0
-		# best_laplace = 0
-		# c = [1, 2, 5, 10, 100, 1000]
-		# for i in c:
-		# 	pclass = {}
-		# 	nclass = {}
-		# 	accuracy = 0
-		# 	Build_Mult(content_topic, pclass, nclass, i)
-		# 	accuracy = Classify_Mult(content_topic_testing, pclass, nclass, topic_p_prior, topic_n_prior)
-			
-		# 	if best_accuracy < accuracy:
-		# 		best_accuracy = accuracy
-		# 		best_laplace = i
-		# print("Best Laplace: ", best_laplace)
-		# print("Binary Conversation Accuracy: ", best_accuracy, "%")
+		print("Binary Conversation Accuracy: ", best_accuracy, "%")
 
 #=======================Using Bernoulli Navie Bayes==============================================
 	if arg == "2":
@@ -81,9 +81,7 @@ def main():
 			accuracy = 0
 			Build_Bernoulli(content_topic, pclass, nclass, word_bank, p_word_bank, n_word_bank, i)
 			accuracy = accuracy = Classify_Bernoulli(content_topic_testing, pclass, nclass, movie_p_prior, movie_n_prior, p_word_bank, n_word_bank, word_bank)
-			# import IPython
-			# IPython.embed()
-			# exit()
+		
 			print("Accuracy ", i, ": ", accuracy, "%")
 			if best_accuracy < accuracy:
 				best_accuracy = accuracy
@@ -120,7 +118,6 @@ def Classify_Mult(content_testing, pclass, nclass, p_prior, n_prior):
 			word = i.split(':')[0]
 			count = i.split(':')[1]
 
-			ipdb.set_trace()
 			if word in pclass:
 				pclass_value += (pclass[word]) * int(count)
 			if word in nclass:
@@ -131,13 +128,9 @@ def Classify_Mult(content_testing, pclass, nclass, p_prior, n_prior):
 	
 		if nclass_value > pclass_value and testlabels == -1:
 			accuracy += 1
-		# import IPython
-		# IPython.embed()
-		# exit()
 
 	accuracy /= len(content_testing)
 	return (accuracy * 100)
-
 
 def Build_Mult(content, pclass, nclass, laplace):
 
@@ -175,21 +168,32 @@ def Build_Mult(content, pclass, nclass, laplace):
 				if word not in set(total_unique):
 					total_unique.extend([word])
 
-	for word in pclass:
-		count = pclass[word]
-		count += laplace
-		count /= (pclass_total + laplace * len(total_unique))
-		pclass[word] = np.log10(count)
-	
-	for word in nclass:
-		count = nclass[word]
-		count += laplace
-		count /= (nclass_total + laplace * len(total_unique))
-		nclass[word] = np.log10(count)
+	for word in total_unique:
+		if word in pclass:
+			count = pclass[word]
+			count += laplace
+			count /= (pclass_total + laplace * len(total_unique))
+			pclass[word] = np.log10(count)
+		else:
+			count = laplace/(pclass_total + laplace * len(total_unique))
+			pclass[word] = np.log10(count)
+
+		if word in nclass:
+			count = nclass[word]
+			count += laplace
+			count /= (nclass_total + laplace * len(total_unique))
+			nclass[word] = np.log10(count)
+		else:
+			count = laplace/(nclass_total + laplace * len(total_unique))
+			nclass[word] = np.log10(count)
 
 def Classify_Bernoulli(content_testing, pclass, nclass, p_prior, n_prior, p_word_bank, n_word_bank, word_bank):
 	accuracy = 0
-
+	remaining = deepcopy(word_bank)
+	import IPython
+	IPython.embed()
+	exit()
+	
 	for line in content_testing:
 		pclass_value = np.abs(np.log(p_prior))
 		nclass_value = np.abs(np.log(n_prior))
@@ -206,17 +210,15 @@ def Classify_Bernoulli(content_testing, pclass, nclass, p_prior, n_prior, p_word
 			count = i.split(':')[1]
 			if word not in word_bank:
 				continue
-
+			remaining.remove(word)
 			if word in pclass:
 				pclass_value += pclass[word]
-			# else:
-			# 	count = 1 - pclass[word]
-			# 	pclass_value *= np.abs(np.log(count))
 			if word in nclass:
 				nclass_value += nclass[word]
-			# else:
-			# 	count = 1 - nclass[word]
-			# 	nclass_value *= np.abs(np.log(count))
+
+		for remain in remaining:
+			pclass_value += pclass[remain]
+			nclass_value += nclass[remain]
 
 		if pclass_value > nclass_value and testlabels == 1:
 			accuracy += 1
@@ -259,19 +261,19 @@ def Build_Bernoulli(content, pclass, nclass, word_bank, p_word_bank, n_word_bank
 		if word in p_word_bank:
 			count = pclass[word]
 			count += laplace
-			count /= (440 + laplace * len(word_bank)) 
+			count /= (440 + laplace * 2)
 			pclass[word] = np.log10(count)
 		else:
-			count = laplace/(440 + laplace * len(word_bank))
+			count = laplace/(440 + laplace * 2)
 			pclass[word] = np.log10(count)
 
 		if word in n_word_bank:
 			count = pclass[word]
 			count += laplace
-			count /= (438 + laplace * len(word_bank))
+			count /= (438 + laplace * 2)
 			nclass[word] = np.log10(count)
 		else:
-			count = laplace/(438 + laplace * len(word_bank))
+			count = laplace/(438 + laplace * 2)
 			nclass[word] = np.log10(count)
 
 main()
