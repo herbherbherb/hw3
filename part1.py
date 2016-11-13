d9b8c8dce600e56e902745c3d1640cae35b13da3
Herbert Wang <rwang67@siebl-0403-34.ews.illinois.edu>
2016-11-13 14:19:54 -0600

    cs440

--- part1.py
+++ part1.py
@@ -54,12 +54,12 @@ def main():
 
 		Accuracy_rate = (correct_predicted/len(testlabels))*100
 		print("Total Accuracy (Laplace = ", poss_laplace[i], "): ", Accuracy_rate, "%")
-		report_digit_accuracy(testresult, testlabels)
+		report_digit_accuracy(testresult, testlabels, NavieDic, trainingset)
 		print("Testing Time: ", end - start_time)
 		print()
 		testresult = []
 
-def report_digit_accuracy(testresult, testlabels):
+def report_digit_accuracy(testresult, testlabels, NavieDic, trainingset):
 	digit = np.zeros((10))
 	correct_digit = np.zeros((10))
 
@@ -80,13 +80,13 @@ def report_digit_accuracy(testresult, testlabels):
 		if i == 3 or i == 6:
 			print()
 	print()
-	confusion_mtx(testresult, testlabels, digit, confusion)
-
-def confusion_mtx(testresult, testlabels, digit, confusion):
+	confusion_mtx(testresult, testlabels, digit, confusion, NavieDic, trainingset)
+	
+def confusion_mtx(testresult, testlabels, digit, confusion, NavieDic, trainingset):
 	for i in range(1, 11):
 		confusion[i, 1:] /= digit[i-1]
 	print()
-	np.set_printoptions(precision=2)
+	np.set_printoptions(precision=2)	
 	print(confusion)
 	print()
 
@@ -98,9 +98,27 @@ def confusion_mtx(testresult, testlabels, digit, confusion):
 		col = np.argmax(np.max(confusion, axis=0))
 		ratiolist.extend([(row, col)])
 		confusion[row, col] = 0
-	print(ratiolist)
 	ratiolist = ratiolist[10:]
-	print(ratiolist)
+	print(ratiolist[4:])
+
+	ratio(NavieDic, ratiolist, trainingset)
+
+def ratio(NavieDic, ratiolist, trainingset):
+	for tup in ratiolist:
+		oddratiomtx = np.zeros((28, 28))
+		num1 = tup[0]
+		num2 = tup[1]
+		for i in range(28):
+			for j in range(28):
+				loc = (i, j)
+				num1count = (NavieDic[loc]['+'][num1] + NavieDic[loc]['#'][num1]+1)/trainingset[num1]
+				num2count = (NavieDic[loc]['+'][num2] + NavieDic[loc]['#'][num2]+1)/trainingset[num2]
+				probability = num1count/num2count
+				oddratiomtx[i, j] = probability
+		print(oddratiomtx)
+		import IPython
+		IPython.embed()
+		exit()
 
 def Building(content, traininglabels, NavieDic):
 	for i in range(int(len(content)/28)):
@@ -143,133 +161,4 @@ def NavieClassify(content, NavieDic, testresult, trainingset, trainingprior, lap
 		max_label = posteriori.index(max(posteriori))
 		testresult.extend([max_label])
 
-# def NavieClassify(content, NavieDic, testresult, trainingset, trainingprior, laplace_const):
-# 	for i in range(int(len(content)/28)):
-
-# 		posteriori = [np.log(trainingprior[i]) for i in range(10)]
-# 		cur_set = deepcopy(trainingset)
-# 		for row in range(i*28, (i+1)*28):
-# 			for col in range(28):
-# 				loc = (row%28, col) 			# location of that grid
-# 				cur_char = content[row][col]	# character in that grid
-			
-# 				classlist = NavieDic[loc][cur_char]
-
-# 				for i in range(10):
-# 					lkhood = classlist[i]
-# 					if lkhood == 0:
-# 						lkhood = laplace_const
-# 						cur_set[i] += laplace_const
-# 					posteriori[i] += np.log(lkhood/cur_set[i])
-# 		max_label = posteriori.index(max(posteriori))
-# 		testresult.extend([max_label])
 main()
-
-
-# import numpy as np
-# from copy import copy, deepcopy
-# import time
-# from collections import defaultdict as setdefault
-
-# def main():
-# #=======initialize================
-# 	traininglabels = []
-# 	testlabels = []
-# 	trainingprior = np.zeros((10))
-# 	likelihood = []
-# 	testresult = []
-
-# 	NavieDic = {}
-# 	NavieDic_prior = {}
-# #========Reading==================
-# 	with open('traininglabels') as f:
-# 		content = f.readlines()
-# 	traininglabels = [int(x.strip('\n')) for x in content]
-
-# 	with open('testlabels') as f:
-# 		content = f.readlines()
-# 	testlabels = [int(x.strip('\n')) for x in content]
-
-
-# 	trainingset = {}						# P(piror) = trainingprior[i] # of class[i] = trainingset
-# 	traininglength = len(traininglabels)
-# 	for i in range(10):
-# 		count = traininglabels.count(i)
-# 		trainingset[i] = count
-# 		trainingprior[i] = count/traininglength
-# 		likelihood.append([])
-
-# 	with open('trainingimages') as f:
-# 		content = f.readlines()
-		
-# 	Building(content, traininglabels, NavieDic)
-# 	Building_piror(content, traininglabels, NavieDic_prior)
-	
-# #===========Testing================================================
-# 	with open('testimages') as f:
-# 		content = f.readlines()
-
-# 	poss_laplace = [0.1, 0.5, 1, 2, 5, 10]
-# 	for i in range(len(poss_laplace)):
-# 		NavieClassify(content, NavieDic, NavieDic_prior, testresult, trainingset, trainingprior, poss_laplace[i])
-# 		correct_predicted = len([i for i, j in zip(testlabels, testresult) if i == j])
-# 		Accuracy_rate = (correct_predicted/len(testlabels))*100
-# 		print("Accuracy (Laplace = ", poss_laplace[i], "): ", Accuracy_rate, "%")
-# 		testresult = []
-
-
-# def Building_piror(content, traininglabels, NavieDic_prior):
-# 	class_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# 	for i in range(int(len(content)/28)):
-# 		cur_label = traininglabels[i]
-# 		for row in range(i*28, (i+1)*28):
-# 			for col in range(28):
-# 				loc = (row%28, col) 			# location of that grid
-
-# 				if loc not in NavieDic_prior:
-# 					NavieDic_prior.setdefault(loc, {})
-# 					for c in class_list:
-# 						NavieDic_prior[loc][c] = 0
-# 					NavieDic_prior[loc][cur_label] += 1
-# 				else:
-# 					NavieDic_prior[loc][cur_label] += 1
-
-# def Building(content, traininglabels, NavieDic):
-# 	for i in range(int(len(content)/28)):
-# 		cur_label = traininglabels[i]
-# 		for row in range(i*28, (i+1)*28):
-# 			for col in range(28):
-# 				loc = (row%28, col) 			# location of that grid
-# 				cur_char = content[row][col]	# character in that grid
-# 				if loc not in NavieDic:			# not in the dictionary yet
-# 					NavieDic.setdefault(loc, {})
-# 					NavieDic[loc][' '] = np.zeros((10))
-# 					NavieDic[loc]['+'] = np.zeros((10))
-# 					NavieDic[loc]['#'] = np.zeros((10))
-# 					NavieDic[loc][cur_char][cur_label] += 1
-# 				else:
-# 					NavieDic[loc][cur_char][cur_label] += 1
-
-# def NavieClassify(content, NavieDic, NavieDic_prior, testresult, trainingset, trainingprior, laplace_const):
-# 	for i in range(int(len(content)/28)):
-
-# 		posteriori = [np.log(trainingprior[i]) for i in range(10)]
-# 		for row in range(i*28, (i+1)*28):
-# 			for col in range(28):
-# 				loc = (row%28, col) 			# location of that grid
-# 				cur_char = content[row][col]	# character in that grid
-			
-# 				classlist = NavieDic[loc][cur_char]
-
-# 				for i in range(10):
-# 					total_num = NavieDic_prior[loc][i]
-# 					lkhood = classlist[i]
-# 					if lkhood == 0:
-# 						lkhood = laplace_const
-# 						total_num += laplace_const * 2
-
-# 					posteriori[i] += np.log(lkhood/total_num)
-# 		max_label = posteriori.index(max(posteriori))
-# 		testresult.extend([max_label])
-
-# main()
